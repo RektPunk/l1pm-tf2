c020a657b4e1ca1d1ba3f6ae235fdb36ecd6234f
RektPunk <rektpunk@gmail.com>
2022-08-11 00:04:56 +0900

    feat - add OutputDense in l1pm

---
+++ l1pm/layers.py
@@ -0,0 +1,39 @@
+import numpy as np
+import tensorflow as tf
+
+
+class l1pmOutputDense(tf.keras.layers.Layer):
+    """
+    Mcqrnn Output dense network
+    *Note that the activation of the last hidden layer must be sigmoid.
+    **Or, Monotone increasing function bounded by [0, 1].
+    Methods:
+        build:
+            Set weight shape for first call
+        call:
+            Return dense layer with activation
+    """
+
+    def __init__(
+        self,
+        n_taus: int,
+        **kwargs,
+    ):
+        super(l1pmOutputDense, self).__init__(**kwargs)
+        self.n_taus = n_taus
+
+    def build(
+        self,
+        input_shape,
+    ):
+        self.w = tf.Variable(tf.random.normal([input_shape[-1], self.n_taus]), name="w")
+        self.b = tf.Variable(tf.zeros([self.n_taus]), name="b")
+        _w_concat = tf.concat([self.b, self.w], axis=0)
+        self.w_cumsum = tf.cumsum(_w_concat)[1:]
+
+    def call(
+        self,
+        inputs: np.ndarray,
+    ):
+        outputs = tf.matmul(inputs, self.w_cumsum) + self.b
+        return outputs
