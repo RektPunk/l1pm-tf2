3210bc03f33e37028dcd9eef49e7da801fd7f559
RektPunk <rektpunk@gmail.com>
2022-08-15 15:11:18 +0900

    feat - fix l1pm structure

--- l1pm/layers.py
+++ l1pm/layers.py
@@ -30,10 +30,12 @@ class l1pmOutputDense(tf.keras.layers.Layer):
     ):
         self.w = tf.Variable(tf.random.normal([input_shape[-1], self.n_taus]), name="w")
         self.b = tf.Variable(tf.zeros([1, self.n_taus]), name="b")
-        _w_concat = tf.concat([self.b, self.w], axis=0)
-        self.w_cumsum = tf.cumsum(_w_concat)[1:]
-        _w_cumsum_reduce_sum = tf.reduce_sum(tf.maximum(0, -self.w_cumsum), axis=0)
-        self.b_adjusted = tf.maximum(self.b, _w_cumsum_reduce_sum)
+
+        _w_cumsum = tf.cumsum(self.w)
+        _w_cumsum_reduce_sum = tf.reduce_sum(tf.maximum(0, -_w_cumsum), axis=0)
+        _b = tf.maximum(self.b, _w_cumsum_reduce_sum)
+        _b_adjusted = tf.concat([self.b[0, :1], _b[0, 1:]], axis=0)
+        self.b_adjusted = tf.reshape(_b_adjusted, [1, self.n_taus])
         self.kernel = self.add_weight(
             "l1pmregularizer",
             shape=[input_shape[-1] + 1, self.n_taus],
